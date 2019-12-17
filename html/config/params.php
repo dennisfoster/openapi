238a6255e9f5435078240223fdd458f5698c5e5e
Dennis Foster <d.foster@accumedintel.com>
2019-12-17 16:44:36 -0700

    Change S3 API key

--- html/config/params.php
+++ html/config/params.php
@@ -4,11 +4,11 @@ return [
     'apiUrlIntranet' => 'https://api.accumedintel.net/',
 
     // Amazon S3 settings
-	'AMAZON_BUCKET' => 'accumedfiles',
-	'AMAZON_FILE_ACCESS_KEY' => 'AKIAI3WANMISWYOGBOPA',
-	'AMAZON_FILE_SECRET_KEY' => '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',
+	'AMAZON_BUCKET' => 'accumed-file-storage',
+	'AMAZON_FILE_ACCESS_KEY' => 'AKIAUZQD5NFCE2MOFTE7',
+	'AMAZON_FILE_SECRET_KEY' => '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',
 
     // mcrypt encryption settings
 	'ENCRYPTION_PASSWORD_KEY' => 'Qp@ceNeldl3',
-	'ENCRYPTION_SALT_KEY' => '!kMm*fN8pZe1Acm%9',    
+	'ENCRYPTION_SALT_KEY' => '!kMm*fN8pZe1Acm%9',
 ];
